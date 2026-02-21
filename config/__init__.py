e03046fbc55f705e5da68fc0fb2badeb137add29
Sayandip Bar <sayandipbar05@gmail.com>
2026-02-22 01:32:33 +0530

    refactor: update config package exports

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,5 @@
-"""Config package for Deep Research Agent."""
+"""Config package - application settings."""
 
 from .settings import settings, get_settings, validate_required_settings
 
-__all__ = [
-    "settings",
-    "get_settings", 
-    "validate_required_settings",
-]
+__all__ = ["settings", "get_settings", "validate_required_settings"]
